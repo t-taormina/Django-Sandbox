a1914f6755ab00e7b5834f7f25a8e360115e8f3f
Tyler Taormina <tylertaormina@Air.local>
2023-02-25 21:21:50 -0800

    adding html files

--- tempProject/tempSite/polls/views.py
+++ tempProject/tempSite/polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = { 'latest_question_list' : latest_question_list }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("Detail View: question_id %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote view: question_id %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("Results view: question_id %s" % question_id)
