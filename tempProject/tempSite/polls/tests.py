38ef52900acb896e9c0883bba62c70517fe73a6c
tyler <tylrr@PRO.local>
2023-03-04 18:57:45 -0800

    Ready to implement Index View tests

--- tempProject/tempSite/polls/tests.py
+++ tempProject/tempSite/polls/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+import datetime
 
 # Create your tests here.
+
+
+def create_question(text, days):
+    """
+    Creates a question with text as the question_text and with an offset from
+    timezone.now(). Positive days will result in future dates. Negative
+    days will result in past dates.
+    """
+    time = timezone.now() + datetime.timedelta(days=offset)
+    return Question.objects.create(question_text=text, pub_date=time)
+
+
+class QuestionModelTests(TestCase):
+
+    def test_future_date_is_not_recent(self):
+        """
+        was_published_recently() returns False for future dates.
+        """
+        future_date = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="Future Question", pub_date=future_date)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_old_question_is_not_recent(self):
+        """
+        was_published_recently() returns False for questions
+        with a pub_date older than 1 day ago.
+        """
+        old_date = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=old_date)
+        self.assertIs(old_question.was_published_recently(), False)
+
+class QuestionIndexViewTests(TestCase):
+    
+    def test_no_questions(self):
+        return 
+
+    def test_future_question(self):
+        return
+
+    def test_future_and_past_questions(self):
+        return
+
+    def test_past_questions(self):
+        return
+
+
+
+
+
--- tempProject/tempSite/polls/views.py
+++ tempProject/tempSite/polls/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone 
+from django.views import generic 
 from .models import Question, Choice
 
 # Create your views here
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = { 'latest_question_list' : latest_question_list }
-    return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return a list containing the 5 most recent published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -30,7 +40,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question' : question})
