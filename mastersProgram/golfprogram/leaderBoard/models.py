7150e73784e4e228115a59db687fcb6846e0db37
t-taormina <tktaormina@gmail.com>
2023-03-05 22:00:14 -0800

    pushing

---
+++ mastersProgram/golfprogram/leaderBoard/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Golfer(models.Model):
+    golfer = models.CharField(max_length=200)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return self.golfer
+
+
+class Player(models.Model):
+    player = models.CharField(max_length=200)
+    golfers = models.ManyToManyField(Golfer)
+
+    def __str__(self):
+        return self.player
+
+
+
